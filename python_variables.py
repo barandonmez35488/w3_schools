d3e22d805f3a757e98b9a11320f1120bb957a369
barandonmez35488 <baran.donmez@akademiata.edu.pl>
2025-04-03 11:29:15 +0200

    python_variables.py

---
+++ python_variables.py
@@ -0,0 +1,9 @@
+x = "Baran"
+y = 2003
+print(type(x))
+print(type(y))
+Baran = "Hello, I am Baran!"
+print('value of baran: baran')
+print("value of baran:",Baran)
+baran = 'baran + 21' 
+print("New value of baran (age):", 'baran')
